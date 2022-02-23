a44a056b59ff216f5baecdf2be44d897165d1488
totallynotdavid <dadch1404@gmail.com>
2022-02-23 08:39:51 -0500

    Fixed 🆔

--- assets/js/docsearch.js
+++ assets/js/docsearch.js
@@ -9,7 +9,7 @@ if (searchPlaceholder !== null) {
 docsearch({
   container: '#docsearch',
   appId: 'JE37E4YOU3',
-  indexName: 'test_CAE',
+  indexName: 'netlify_abc167f5-8dee-48db-a7da-3c97c18849be_master_all',
   apiKey: '0b06babcb9d1b0aaadaa9e9af5be7bfe',
   debug: false,
 });
